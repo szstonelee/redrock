ceca1f4ca009005de7665959264fe0bd4254d368
szstonelee <szstonelee@Tongs-MacBook-Pro.local>
2022-04-28 21:43:27 +0800

    sample_for_redrock

--- docs/source-build.md
+++ docs/source-build.md
@@ -6,18 +6,18 @@
 
 RedRock需要最低gcc/g++7编译环境，如果未安装，可以安装最新的gcc/g++
 
-#### CentOS
+#### CentOS安装编译环境
 
-#### Ubuntu
+#### Ubuntu安装编译环境
 
-#### MacOS
+#### MacOS安装编译环境
 
 
-### 下载和编译支持库
+### 下载和编译支持库lz4和RocksDB
 
 需要至少两个库的支持，先是lz4，然后是RocksDB（RocksDB需要知道lz4安装成功）
 
-#### lz4
+#### 安装lz4支持库
 
 ```
 git clone https://github.com/lz4/lz4.git
@@ -27,13 +27,37 @@ make install
 ```
 注：上面的github.com可以替换为hub.fastgit.xyz
 
-#### RocksDB
+可以检查lz4是否成功
+
+对于Linux
+```
+find /usr -name liblz4.so
+```
+
+对于Mac
+```
+find /usr -name liblz4.dylib
+```
+
+#### 安装RocksDB支持库
 
 ```
 git clone -b v7.2.0 https://github.com/facebook/rocksdb.git
 cd rocksdb
 make shared_lib
 make install
+```
+
+可以检查RocksDB是否成功
+
+对于Linux
+```
+find /usr -name librocksdb.so
+```
+
+对于Mac
+```
+find /usr -name librocksdb.dylib
 ```
 
 ### 下载RedRock源代码
---
+++ tests/rock/gen_some_str_keys.py
@@ -0,0 +1,39 @@
+import sys
+import time
+
+import redis
+import random
+
+# This is for a simple sample test for RedRock for Readme.md
+# It will generage some string keys in database 0 of RedRock
+
+r_ip = "127.0.0.1"
+r_port = 6379
+pool = redis.ConnectionPool(host=r_ip,
+                            port=r_port,
+                            db=0,
+                            decode_responses=True,
+                            encoding='latin1',
+                            socket_connect_timeout=2)
+r: redis.StrictRedis = redis.StrictRedis(connection_pool=pool)
+
+
+def _main():
+    num = int(sys.argv[1])
+    if num < 0:
+        exit(1)
+    for i in range(1, num+1):
+        key = "k" + str(i)
+        val_len = random.randint(2, 2000)
+        val = str(val_len) + "v" * val_len
+        cmd = f"set {key} {val}"
+        r.execute_command(cmd)
+        if i % 1000 == 1:
+            print(f"gen key i = {i}, time = {time.time()}")
+    r.execute_command("save")
+    print(f"Generate {num} string keys in RedRock success! Now you can use dump.rdb.")
+
+
+if __name__ == '__main__':
+    _main()
+
